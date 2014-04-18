ec0fd312280279133b58984b4ad55060cfb82f27
lokesh <eshi42@gmail.com>
2014-04-18 12:46:45 +0530

    Initial Commit

---
+++ LokeshDemoServer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LokeshDemoServer
+//
+//  Created by jassal on 18/04/14.
+//  Copyright (c) 2014 Lokesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LokeshDemoServer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LokeshDemoServer
+//
+//  Created by jassal on 18/04/14.
+//  Copyright (c) 2014 Lokesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
