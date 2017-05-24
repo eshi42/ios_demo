44f214ecafbfe679fb61f318b0f70e8a36a9f303
Lokesh kumar <lokesh.kumar@kabuni.com>
2017-05-24 10:57:05 +0530

    commmittt

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 var express = require('express')
 var app = express()
-
+var port = Number(process.env.PORT || 8000);
 app.use(express.static('public'));
 
 app.get('',function (req,response) {
 	response.sendFile(__dirname + '/' + 'index.html')
 })
 
-var server = app.listen(function() {
+var server = app.listen(port,function() {
 	var host = server.address().address
-	var port = server.address().port
+	//var port = server.address().port
 		  	console.log('listening at http://%s:%s',host,port)
 	});
